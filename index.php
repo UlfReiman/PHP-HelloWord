fa22b143fc70957e7074c598e3e7c9f6dd23adc6
UlfReiman <ulf.reiman@gmail.com>
2019-12-03 10:55:07 +0100

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
   <title>PHP Testing</title>
  </head>
  <body>
- <?php echo '<p>111111Hello World-Happy Learning. This is ! This is my first Jenkins PHP200 page using OpenShift Online</p>'; ?> 
+ <?php echo '<p>222Hello World-Happy Learning. This is ! This is my first Jenkins PHP200 page using OpenShift Online</p>'; ?> 
  </body>
 </html>
